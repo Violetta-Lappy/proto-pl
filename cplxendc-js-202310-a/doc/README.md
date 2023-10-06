bc043f175f2d231c6193be9ebc9205fbe86de1c7
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-10-06 14:46:36 +0700

    Update Stuffs

--- cplxendc-js-202310-a/doc/README.md
+++ cplxendc-js-202310-a/doc/README.md
@@ -6,15 +6,40 @@
 # Table of Contents
 - [CPLXENDC](#cplxendc)
 - [Table of Contents](#table-of-contents)
+- [Inspiration](#inspiration)
+- [Introduction](#introduction)
+- [Important](#important)
 - [Features](#features)
+- [Design System](#design-system)
 - [Folder](#folder)
 - [Build System](#build-system)
+- [Include](#include)
 - [Variable Keywords](#variable-keywords)
 
+# Inspiration
+- Flutter / Dart
+- Rust
+- WebGPU
+- IBM Carbon Design System
+- Renderer opposed to Webview
+
+# Introduction
+cplxendc is a programming language designed for high performance (including pointer manipulation)
+- Backend only
+- WebGPU for most of the interaction
+
+HTML / CSS / JS / HTMX
+- Frontend GUI that interact with cplxendc process / function scripts
+
+# Important
+Please note that cplxendc will never use webview, but rather render the damn thing
+DO NOT ADD UNNECESSARY FEATURES OR BLOAT STUFFS
+- Desktop Application - 2x_build_desktop - convert .cplxendc to work with C++ with WebGPU
+- Web Application - 2x_build_web - convert .cplxendc to work with JS / HTML / CSS / HTMX
+- Mobile Application - C++ with WebGPU (I don't if this will ever supported)
+
 # Features
-- #include <main.js> //no bullshit
 - .cplxendc / 2x build tool
-- cplxendc_gui / cplxendc_carbon - inspired from IBM Carbon (single file and minimal performance) - imgui & retained
 - header / class / pstt (processtrait)
 - process (ps) / function (fn) - first word last word
 - async / const / mut
@@ -22,19 +47,31 @@
 - let something = 0;
 - always end with ; (end at) or ! (priority, more just means below like header, max limit 3, only usable in process or function)
 
+# Design System
+- cplxendc_carbon - inspired from IBM Carbon (single file and minimal performance)
+- cplxendc_gui - retained or imgui style
+- cplxendc_gui_bind - bind against html and htmx
+
 # Folder
-- data
-  - font
-  - audio
-- src
-- 3rdparty (external, vendor)
+User create destination themself in the .2xconfig
+
+Require the following folder to be create:
+- data (contain font, audio)  
+- src (source code)
+- 3rdparty (package, external, vendor)
 
 # Build System
 - 2x command (2x main.cplxendc -o output)
 - .2xconfig (work like cmake but more towards npm javascript and premake)
 
+# Include
+Use the main C++ but without bullshit syntax, a oneway to write this thing 
+- #include <main.cplxendc> - use for in the same folder only, not affected by global config
+- #include <path/main.cplxendc> - use specific folder based on config
+- #include <path/main.cplxendc> from url - use for something on web based on config
+
 # Variable Keywords
-- let
+- let / var
 - int
 - float
 - double
--- cplxendc-js-202310-a/index.js
+++ cplxendc-js-202310-a/index.js
@@ -1,20 +1,12 @@
 /*
 CPLXENDC - Release A - 20231006
-- #include <main.js> //no bullshit
-- .cplxendc / 2x build tool
-- cplxendc_gui / cplxendc_carbon - inspired from IBM Carbon (single file and minimal performance) - imgui & retained
-- process (ps) / function (fn) - first word last word
-- async / const / mut
-- private / public / protected
-- var / int / float / string
-- let something = 0;
 */
 
 class cplxendc {
     constructor(text) {
         this.text = text;
     }
-    tokenize(){
+    tokenize() {
         const length = this.text.length;
         let pos = 0;
         let tokens = [];
@@ -22,27 +14,27 @@ class cplxendc {
         const varChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
         let line = 1;
         let column = 0;
-        while(pos < length){
-            let currentChar = this.codes[pos]
-            if(currentChar === " "){
+        while (pos < length) {
+            let currentChar = this.text[pos]
+            if (currentChar === " ") {
                 pos++
                 column++
                 continue
-            }else if(currentChar === "\n"){
+            } else if (currentChar === "\n") {
                 line++
                 column = 0
                 pos++
                 continue
-            }else if(currentChar === '"'){
+            } else if (currentChar === '"') {
                 let res = ""
                 pos++
                 column++
-                while(this.codes[pos] !== '"' && pos < length){
-                    res += this.codes[pos]
+                while (this.text[pos] !== '"' && pos < length) {
+                    res += this.text[pos]
                     pos++
                     column++
                 }
-                if(this.codes[pos] !== '"'){
+                if (this.text[pos] !== '"') {
                     return {
                         error: `Unterminated string at line ${line} column ${column}`
                     }
@@ -53,12 +45,12 @@ class cplxendc {
                     type: "string",
                     value: res,
                 })
-            }else if(varChars.includes(currentChar)){
+            } else if (varChars.includes(currentChar)) {
                 let res = currentChar
                 pos++
                 column++
-                while(varChars.includes(this.codes[pos]) && pos < length){
-                    res += this.codes[pos]
+                while (varChars.includes(this.text[pos]) && pos < length) {
+                    res += this.text[pos]
                     pos++
                     column++
                 }
@@ -66,16 +58,16 @@ class cplxendc {
                     type: BUILT_IN_KEYWORDS.includes(res) ? "keyword" : "keyword_custom",
                     value: res
                 })
-            }else if(currentChar === "="){
+            } else if (currentChar === "=") {
                 pos++
                 column++
                 tokens.push({
                     type: "operator",
                     value: "eq"
                 })
-            }else{
+            } else {
                 return {
-                    error: `Unexpected character ${this.codes[pos]} at line ${line} column ${column}`
+                    error: `Unexpected character ${this.text[pos]} at line ${line} column ${column}`
                 }
             }
         }
@@ -84,35 +76,35 @@ class cplxendc {
             tokens
         }
     }
-    parse(tokens){
+    parse(tokens) {
         const len = tokens.length
         let pos = 0
         const vars = {}
 
-        while(pos < len){
+        while (pos < len) {
             const token = tokens[pos]
-            if(token.type === "keyword" && token.value === "print"){
-                if(!tokens[pos + 1]){
+            if (token.type === "keyword" && token.value === "print") {
+                if (!tokens[pos + 1]) {
                     return console.log("Unexpected end of line, expected string")
                 }
                 let isVar = tokens[pos + 1].type === "keyword_custom"
                 let isString = tokens[pos + 1].type === "string"
-                if(!isString && !isVar){
+                if (!isString && !isVar) {
                     return console.log(`Unexpected token ${tokens[pos + 1].type}, expected string`)
                 }
-                if(isVar){
-                    if(!(tokens[pos + 1].value in vars)){
+                if (isVar) {
+                    if (!(tokens[pos + 1].value in vars)) {
                         return console.log(`Undefined variable ${tokens[pos + 1].value}`)
                     }
                     console.log('\x1b[35m%s\x1b[0m', vars[tokens[pos + 1].value])
-                }else{
+                } else {
                     console.log('\x1b[35m%s\x1b[0m', tokens[pos + 1].value)
                 }
                 pos += 2
-            }else if(token.type === "keyword" && token.value === "var"){
+            } else if (token.type === "keyword" && token.value === "var") {
                 const isCustomKW = tokens[pos + 1] && tokens[pos + 1].type === "keyword_custom"
-                if(!isCustomKW){
-                    if(!tokens[pos + 1]){
+                if (!isCustomKW) {
+                    if (!tokens[pos + 1]) {
                         return console.log("Unexpected end of line, expected variable name")
                     }
                     return console.log(`Unexpected token ${tokens[pos + 1].type}, expected variable name`)
@@ -120,45 +112,47 @@ class cplxendc {
                 const varName = tokens[pos + 1].value
 
                 const isEq = tokens[pos + 2] && tokens[pos + 2].type === "operator" && tokens[pos + 2].value === "eq"
-                if(!isEq){
-                    if(!tokens[pos + 2]){
+                if (!isEq) {
+                    if (!tokens[pos + 2]) {
                         return console.log("Unexpected end of line, expected =")
                     }
                     return console.log(`Unexpected token ${tokens[pos + 1].type}, expected =`)
                 }
 
                 const isString = tokens[pos + 3] && tokens[pos + 3].type === "string"
-                if(!isString){
-                    if(!tokens[pos + 3]){
+                if (!isString) {
+                    if (!tokens[pos + 3]) {
                         return console.log("Unexpected end of line, expected string")
                     }
                     return console.log(`Unexpected token ${tokens[pos + 1].type}, expected string`)
                 }
 
-                if(varName in vars){
+                if (varName in vars) {
                     return console.log(`Variable ${varName} already exists`)
                 }
                 vars[varName] = tokens[pos + 3].value
                 pos += 4
-            }else{
+            } else {
                 return console.log(`Unexpected token ${token.type}`)
             }
         }
     }
 
-    init() { console.log("CPLXENDC LOG: initalize"); } 
+    init() { console.log("CPLXENDC LOG: initalize"); }
 
     run() {
-        console.log(this.text);
+        const { tokens, error } = this.tokenize()
+        if(error){
+            return console.log(error)
+        }
+        this.parse(tokens)
     }
 
     shutdown() { console.log("CPLXENDC LOG: shutdown"); }
 }
 
-const str_code = 
-`fn HelloWorld() : void {
-    print("hello world");
-}
+const str_code =
+`print "hello world"
 `
 
 const program = new cplxendc(str_code);
