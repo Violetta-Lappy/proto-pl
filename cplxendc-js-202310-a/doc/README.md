ea4792287edec9a9a130ae66b65ba33159ae57b7
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-10-06 07:38:27 +0700

    Update Stuffs

---
+++ cplxendc-js-202310-a/doc/README.md
@@ -0,0 +1,29 @@
+# CPLXENDC
+- Release Prefix : A
+- Version Year Month: 202310
+- Last Update: 20231006
+
+# Table of Contents
+- [CPLXENDC](#cplxendc)
+- [Table of Contents](#table-of-contents)
+- [Features](#features)
+- [Variable Keywords](#variable-keywords)
+
+# Features
+- #include <main.js> //no bullshit
+- .cplxendc / 2x build tool
+- cplxendc_gui / cplxendc_carbon - inspired from IBM Carbon (single file and minimal performance) - imgui & retained
+- header / class / pstt (processtrait)
+- process (ps) / function (fn) - first word last word
+- async / const / mut
+- private / public / protected
+- let something = 0;
+- always end with ; (end at) or ! (priority, more just means below like header, max limit 3, only usable in process or function)
+
+# Variable Keywords
+- let
+- int
+- float
+- double
+- char
+- string
--- cplxendc-js-202310-a/index.js
+++ cplxendc-js-202310-a/index.js
@@ -1,15 +1,29 @@
 /*
 CPLXENDC - Release A - 20231006
+- #include <main.js> //no bullshit
 - .cplxendc / 2x build tool
-- process / function
+- cplxendc_gui / cplxendc_carbon - inspired from IBM Carbon (single file and minimal performance) - imgui & retained
+- process (ps) / function (fn) - first word last word
 - async / const / mut
 - private / public / protected
 - var / int / float / string
+- let something = 0;
 */
 
 class cplxendc {
     constructor(text) {
         this.text = text;
+    }
+    tokenize(){
+        const length = this.text.length;
+        let pos = 0;
+        let tokens = [];
+        const BUILT_IN_KEYWORDS = ["print", "var", "let"];
+        const varChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
+        let line = 1;
+        let column = 0;
+    }
+    parse(){        
     }
     init() {
         console.log("CPLXENDC LOG: initalize");
@@ -22,9 +36,10 @@ class cplxendc {
     }
 }
 
-const str_code = `
-print "hello world";
-screwyou
+const str_code = 
+`ps HelloWorld() : void {
+    print("hello world");
+}
 `
 
 const program = new cplxendc(str_code);
