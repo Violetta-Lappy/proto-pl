f98f79c80a19e1989b4b571420727ffef9abb2de
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-10-06 17:17:54 -0700

    Add project

---
+++ cplxendc-csharp-202310-a/cplxendc-csharp-202310-a/SyntaxKind.cs
@@ -0,0 +1,17 @@
+﻿namespace CPLXENDC {
+    enum SyntaxKind {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression,
+        ParenthesizedExpression
+    }
+}
---
+++ cplxendc-csharp-202310-a/cplxendc-csharp-202310-a/SyntaxNode.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace CPLXENDC {
+    abstract class SyntaxNode {
+        public abstract SyntaxKind Kind { get; }
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+}
